af9ebb4b663d689e081518b1127b50baf9139a1f
Jeff Merckens <jeff.merckens@gmail.com>
2023-08-05 17:38:38 -0400

    messing with the canvas

--- src/canvas.js
+++ src/canvas.js
@@ -10,63 +10,107 @@ let radius = 10;
 // Change in position of the ball
 let dx = 2;
 let dy = -2;
-let timer = 1;
+let time = 0;
 
-const paddleHeight = 10;
-const paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) / 2;
+const shipHeight = 10;
+const shipWidth = 25;
+let shipX = (canvas.width - shipWidth) / 2;
+let shipY = (canvas.height - shipHeight) / 2;
+
+let asteroids = [];
 
 let rightPressed = false;
 let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
 
-// Draw ball centered at (x, y)
-function drawBall() {
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText(`Elapsed Time: ${time}`, 8, 20);
+}
+
+// Draws the asteroid
+function drawAsteroid(anAsteroid) {
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fillStyle = "red";
+    ctx.arc(anAsteroid.x, anAsteroid.y, anAsteroid.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
 }
 
-function drawPaddle() {
+// Create an asteroid at the top of the canvas
+function spawnAsteroid() {
+    const x = Math.floor(Math.random() * canvas.width);
+    const radius = Math.min(Math.floor(Math.random() * 50), 10);
+    const dx = Math.floor(Math.random() * 20) - 10;
+    const dy = Math.floor(Math.random() * 10);
+
+    // Save the asteroid so it can be drawn each frame
+    const theAsteroid = {
+        dx: dx,
+        dy: dy,
+        radius: radius,
+        x: x,
+        y: 0,
+    };
+
+    drawAsteroid(theAsteroid);
+    asteroids.push(theAsteroid);
+}
+
+// Moves each asteroid based on its speed
+function moveAsteroids() {
+    asteroids.forEach((asteroid) => {
+        asteroid.x = asteroid.x + asteroid.dx;
+        asteroid.y = asteroid.y + asteroid.dy;
+
+        if (asteroid.x < 0
+            || asteroid.x > canvas.width
+            || asteroid.y < 0
+            || asteroid.y > canvas.height) {
+            asteroid['offscreen'] = true;
+        }
+    });
+
+    asteroids = asteroids.filter((asteroid) => !asteroid.offscreen);
+}
+
+function drawship() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.rect(shipX, shipY, shipWidth, shipHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
-// Move the ball
-function moveBall() {
-    // Detect collision with left or right side
-    if (x < 0 + radius || x > canvas.width - radius) {
-        // Reverse direction on the X axis
-        dx *= -1;
+// Move the ship based on which buttons are pressed
+function moveShip() {
+    if (rightPressed) {
+        shipX = Math.min(shipX + 7, canvas.width - shipWidth);
+    } else if (leftPressed) {
+        shipX = Math.max(shipX - 7, 0);
     }
+    if (upPressed) {
+        shipY = Math.max(shipY - 7, 0);
+    } else if (downPressed) {
+        shipY = Math.min(shipY + 7, canvas.height - shipHeight);
+    }
+}
 
-    // Detect collision with top or bottom side
-    if (y < 0 + radius) {
-        // Reverse direction on the Y axis
-        dy *= -1;
-    } else if (y > canvas.height - radius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
-            dy *= -1;
-        } else {
+// Check if the ship collided with any of the asteroids
+function checkForCollisions() {
+    asteroids.forEach((asteroid) => {
+        if ((shipX >= asteroid.x - asteroid.radius)
+            && (shipX <= asteroid.x + asteroid.radius)
+            && (shipY >= asteroid.y - asteroid.radius)
+            && (shipY <= asteroid.y + asteroid.radius)) {
+            // collision
             alert("GAME OVER");
             document.location.reload();
             clearInterval(interval); // Needed for Chrome to end game
         }
-    }
-
-    // Advance the center of the circle
-    x += dx;
-    y += dy;
-
-    // Increase speed every 100 frames
-    if (timer % 100 === 0) {
-        dx *= 1.1;
-        dy *= 1.1;
-    }
+    });
 }
 
 // Draw a frame of the game 
@@ -74,35 +118,46 @@ function draw() {
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawBall();
-    drawPaddle();
-    moveBall();
+    drawScore();
+    if (time % 1000 === 0) spawnAsteroid();
+    drawship();
+    moveAsteroids();
+    asteroids.forEach((asteroid) => drawAsteroid(asteroid));
 
-    if (rightPressed) {
-        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
-    } else if (leftPressed) {
-        paddleX = Math.max(paddleX - 7, 0);
-    }
+    moveShip();
+    checkForCollisions();
 
-    timer += 1;
+    time += 10;
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
+    console.debug('keyDownHandler: ', e);
+
     if (e.key === "Right" || e.key === "ArrowRight") {
         rightPressed = true;
     } else if (e.key === "Left" || e.key === "ArrowLeft") {
         leftPressed = true;
+    } else if (e.key === "Up" || e.key === "ArrowUp") {
+        upPressed = true;
+    } else if (e.key === "Down" || e.key === "ArrowDown") {
+        downPressed = true;
     }
 }
 
 function keyUpHandler(e) {
+    console.debug('keyUpHandler', e);
+
     if (e.key === "Right" || e.key === "ArrowRight") {
         rightPressed = false;
     } else if (e.key === "Left" || e.key === "ArrowLeft") {
         leftPressed = false;
+    } else if (e.key === "Up" || e.key === "ArrowUp") {
+        upPressed = false;
+    } else if (e.key === "Down" || e.key === "ArrowDown") {
+        downPressed = false;
     }
 }
 
